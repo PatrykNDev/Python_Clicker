3e134f69566694c79a58e735dd26a7f4313e0487
PatrykNDev <96073369+PatrykNDev@users.noreply.github.com>
2022-01-05 20:42:23 +0100

    Patch 5
    *Lepszy sklep z ulepszeniami
    *Nowe napisy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys, pygame
 import random
 
+from pygame.display import set_caption
+
 pygame.init()
 pygame.font.init()
 pygame.display.init()
@@ -12,6 +14,7 @@ pink = 255, 100, 100
 black = 0,0,0
 
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Clicker z Questami')
 clock = pygame.time.Clock()
 pygame.time.set_timer(pygame.USEREVENT, 1000)
 
@@ -24,33 +27,12 @@ credit = 0
 def_inc = 1
 my_font = pygame.font.SysFont(None, 30)
 little_font = pygame.font.SysFont(None, 15)
+smaller_font = pygame.font.SysFont(None, 25)
 my_text = my_font.render(f'Twoje zebrane punkty: {credit}', 1, blue)
 q_text = my_font.render(f'Brak aktywnej misji :(', 1, blue)
 
 
-def shop_draw(pet_sit_cost: int, clicked: bool, increment_per_s: int) -> int:
-    #pet_sit_cost = 10
-    pet_sit_upgrade = 1
-    if clicked:
-        pet_sit_cost*=2
-        increment_per_s+=pet_sit_upgrade
-    #Rysowanie sklepu
-    pet_sitter = pygame.image.load("pet_sitter.gif")
-    pet_sit_rect=pet_sitter.get_rect()
-    pet_sit_rect.x = 820
-    pet_sit_rect.y = 80
 
-    shop_text = my_font.render('Sklep z ulepszeniami', 1, pink)
-    pet_sit_text = my_font.render(f'Koszt: {pet_sit_cost}', 1, blue)
-    pet_sit_text2 = little_font.render(f'Daje + {pet_sit_upgrade}/s dodatkowych punktów', 1, blue)
-
-    pygame.draw.rect(screen, blue, pygame.Rect(815, 75, screen.get_width() - 815, screen.get_height() - 145), 1)
-    pygame.draw.rect(screen, blue, pygame.Rect(815, 40, screen.get_width() - 815, 30), 1)
-    screen.blit(shop_text, (820, 45))
-    screen.blit(pet_sitter, pet_sit_rect)
-    screen.blit(pet_sit_text, (pet_sit_rect.x+pet_sit_rect.width + 5, pet_sit_rect.y + 5))
-    screen.blit(pet_sit_text2, (pet_sit_rect.x + pet_sit_rect.width + 5, pet_sit_rect.y + 35))
-    return pet_sit_cost, increment_per_s
 
 
 def mission(screen: pygame.Surface, credit: int):
@@ -70,7 +52,7 @@ def mission(screen: pygame.Surface, credit: int):
         dog_rect.y = 100
         if quest_started:
             if progress > 0:
-                progress -= 0.009  # 0.001
+                progress -= 0.001  # 0.009
             else:
                 return credit, mission_passed
         for event in pygame.event.get():
@@ -125,12 +107,76 @@ def mission(screen: pygame.Surface, credit: int):
             return credit, mission_passed
 
 
+def draw_texts(points_on_click: int, points_per_second: int) -> None:
+    font_19 = pygame.font.SysFont(None, 19)
+    points_on_click_txt = font_19.render(f'Punkty od kliknięcia: +{points_on_click}', 1, blue)
+    points_per_second_txt = font_19.render(f'Punkty na sekundę: +{points_per_second}',1,blue)
+
+    pygame.draw.rect(screen, blue, pygame.Rect(15, 45, points_on_click_txt.get_width() + 10, 25), 1)
+    screen.blit(points_on_click_txt, (20, 50))
+
+    pygame.draw.rect(screen, blue, pygame.Rect(15, 70, points_on_click_txt.get_width() + 10, 21), 1)
+    screen.blit(points_per_second_txt, (20, 72))
+    pass
+
+
+def shop_draw(pet_sit_cost: int, clicked: bool, increment_per_s: int) -> int:
+    #pet_sit_cost = 10
+    pet_sit_upgrade = 1
+    if clicked:
+        pet_sit_cost*=2
+        increment_per_s+=pet_sit_upgrade
+    #Rysowanie sklepu
+    pet_sitter = pygame.image.load("pet_sitter.gif")
+    pet_sit_rect=pet_sitter.get_rect()
+    pet_sit_rect.x = 820
+    pet_sit_rect.y = 110
+
+    shop_text = my_font.render('Sklep z ulepszeniami', 1, pink)
+    pet_sit_text = my_font.render(f'Koszt: {pet_sit_cost}', 1, blue)
+    pet_sit_text2 = little_font.render(f'Daje + {pet_sit_upgrade}/s dodatkowych punktów', 1, blue)
+
+    pygame.draw.rect(screen, blue, pygame.Rect(815, 105, screen.get_width() - 815, screen.get_height() - 145), 1)
+    pygame.draw.rect(screen, blue, pygame.Rect(815, 70, screen.get_width() - 815, 30), 1)
+    screen.blit(shop_text, (820, 75))
+    screen.blit(pet_sitter, pet_sit_rect)
+    screen.blit(pet_sit_text, (pet_sit_rect.x+pet_sit_rect.width + 5, pet_sit_rect.y + 5))
+    screen.blit(pet_sit_text2, (pet_sit_rect.x + pet_sit_rect.width + 5, pet_sit_rect.y + 35))
+
+    return pet_sit_cost, increment_per_s
+
+
+def draw_second_upgrade(click_upg_cost: int, clicked: bool, increment_on_click: int) -> int:
+    # pet_sit_cost = 10
+    click_upgrade = 1
+    if clicked:
+        click_upg_cost *= 2
+        increment_on_click += click_upgrade
+    # Rysowanie sklepu
+    click_upgr_icon = pygame.image.load("on_click_upgrade.gif")
+    click_upgr_rect = click_upgr_icon.get_rect()
+    click_upgr_rect.x = 820
+    click_upgr_rect.y = 246
+
+    pet_sit_text = my_font.render(f'Koszt: {click_upg_cost}', 1, blue)
+    pet_sit_text2 = little_font.render(f'Daje + {click_upgrade} punktów na kliknięcie', 1, blue)
+
+    pygame.draw.rect(screen, blue, pygame.Rect(815, 70, screen.get_width() - 815, 30), 1)
+    screen.blit(click_upgr_icon, click_upgr_rect)
+    screen.blit(pet_sit_text, (click_upgr_rect.x + click_upgr_rect.width + 5, click_upgr_rect.y + 5))
+    screen.blit(pet_sit_text2, (click_upgr_rect.x + click_upgr_rect.width + 5, click_upgr_rect.y + 35))
+
+    return click_upg_cost, increment_on_click
+
+
 quest1_started = False
 click = False
 click_time = 0
-pet_sit_cost = 10
+pet_sit_cost = 500 #500
 pet_sit_clicked = False
 inc_per_s = 0
+click_upg_cost = 250 #250
+click_upg_clicked = False
 while 1:
 
     dog_rect.x = 40
@@ -147,10 +193,17 @@ while 1:
                 click_text = my_font.render(f'+ {def_inc}', 1, blue)
                 click = True
                 click_time = 10
-            elif 820 <= x <= 1034 and 80 <= y <= 211 and credit >= pet_sit_cost:
+            elif 820 <= x <= 1034 and 110 <= y <= 241 and credit >= pet_sit_cost:
                 credit -= pet_sit_cost
                 my_text = my_font.render(f'Twoje zebrane punkty: {credit}', 1, blue)
                 pet_sit_clicked = True
+            elif 820 <=x <= 1034 and 246 <= y <= 460 and credit >= click_upg_cost:
+                credit -= click_upg_cost
+                my_text = my_font.render(f'Twoje zebrane punkty: {credit}', 1, blue)
+                click_upg_clicked = True
+        if event.type == pygame.USEREVENT:
+            credit += inc_per_s
+            my_text = my_font.render(f'Twoje zebrane punkty: {credit}', 1, blue)
     # if credit == 5:
     if credit == 5 and quest1_started == False:
         quest1_started = True
@@ -158,7 +211,7 @@ while 1:
         my_text = my_font.render(f'Twoje zebrane punkty: {credit}', 1, blue)
         q_text = my_font.render(f'Brak aktywnej misji :(', 1, blue)
         if mission_passed:
-            m1_text = my_font.render('Przeszedles pierwsza misje i twoje kliknięcia są teraz warte o 1 punkt więcej', 1,
+            m1_text = smaller_font.render('Przeszedles pierwsza misje i twoje kliknięcia są teraz warte o 1 punkt więcej', 1,
                                      blue)
             def_inc += 1
         else:
@@ -168,14 +221,18 @@ while 1:
     if click_time > 0:
         click_time -= 1
 
-    credit+=inc_per_s
-    my_text = my_font.render(f'Twoje zebrane punkty: {credit}', 1, blue)
+
 
     screen.blit(background, bcgRect)
     screen.blit(dog, dog_rect)
 
     pet_sit_cost, inc_per_s = shop_draw(pet_sit_cost,pet_sit_clicked, inc_per_s)
     pet_sit_clicked = False
+
+    click_upg_cost, def_inc = draw_second_upgrade(click_upg_cost,click_upg_clicked,def_inc)
+    click_upg_clicked = False
+
+    draw_texts(def_inc, inc_per_s)
 
     pygame.draw.rect(screen, blue, pygame.Rect(495, 5, q_text.get_width() + 10, 30), 1)
     screen.blit(q_text, (500, 10))
